be7adfd2a6cdf59a5bc60228259cbe9266e2f01c
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-15 18:25:46 +0900

    Add Dependency & Notification Permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Permission
+    implementation(libs.accompanist.permissions)
+
 }
