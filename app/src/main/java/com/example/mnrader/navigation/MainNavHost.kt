421fad5610d8a5cbab5f24368044a676a8fcd5ce
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-22 12:39:05 +0900

    feat: Routes 추가 #1

--- app/src/main/java/com/example/mnrader/navigation/MainNavHost.kt
+++ app/src/main/java/com/example/mnrader/navigation/MainNavHost.kt
@@ -5,19 +5,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun MainNavHost(
-    padding : PaddingValues,
+    padding: PaddingValues,
 ) {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = "Main",
+        startDestination = Routes.MAIN,
         modifier = Modifier.padding(padding),
     ) {
 
     }
+}
+
+object Routes {
+    // 온보딩, 회원가입, 로그인, 메인화면, 애니멀페이지, 알림 , add
+    const val ONBOARDING = "onboarding"
+    const val REGISTER = "register"
+    const val LOGIN = "login"
+    const val MAIN = "main"
+    const val ANIMAL_DETAIL = "animal_detail"
+    const val NOTIFICATION = "notification"
+    const val ADD = "add"
+    const val MYPAGE = "mypage"
+    const val SETTING = "setting"
 }
