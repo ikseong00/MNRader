ab55ecbe9b38d3b379c4d24cf14c6e83fbdcb0c9
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-28 17:57:04 +0900

    feat: HomeTopBar 컴포넌트 구현 #11

---
+++ app/src/main/java/com/example/mnrader/ui/home/component/HomeTopBar.kt
@@ -0,0 +1,70 @@
+package com.example.mnrader.ui.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mnrader.R
+import com.example.mnrader.ui.theme.MNRaderTheme
+
+@Composable
+fun HomeTopBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(51.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(start = 6.dp)
+                .size(50.dp),
+            painter = painterResource(R.drawable.img_logo_onboarding),
+            contentDescription = null,
+        )
+        Row {
+            Icon(
+                modifier = modifier.size(24.dp),
+                painter = painterResource(R.drawable.ic_home_pin),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+            Text(
+                text = "서울 광진구",
+                style = MNRaderTheme.typography.regular.copy(
+                    fontSize = 16.sp,
+                    lineHeight = 20.sp,
+                )
+            )
+        }
+
+        Icon(
+            modifier = modifier
+                .padding(end = 18.dp)
+                .size(24.dp),
+            painter = painterResource(R.drawable.ic_home_notification),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeTopBarPreview() {
+    HomeTopBar()
+}
