8f2b4616c19c38089fccd64892ed0756ce11ae50
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-28 17:50:03 +0900

    chore: import 정리 #9

--- app/src/main/java/com/example/mnrader/MainActivity.kt
+++ app/src/main/java/com/example/mnrader/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.example.mnrader
 
-import android.net.http.SslCertificate.restoreState
-import android.net.http.SslCertificate.saveState
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
