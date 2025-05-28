bccd012249668fc6748243c9d48823fec7e8b5de
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-28 18:20:38 +0900

    feat: HomeFilter 컴포넌트 구현 #11

---
+++ app/src/main/java/com/example/mnrader/ui/home/component/HomeFilter.kt
@@ -0,0 +1,56 @@
+package com.example.mnrader.ui.home.component
+
+import android.R.attr.onClick
+import android.R.attr.text
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mnrader.ui.common.DropdownFilterChip
+import com.example.mnrader.ui.common.FilterChip
+
+@Composable
+fun HomeFilter(
+    modifier: Modifier = Modifier,
+) {
+    val scrollState = rememberScrollState()
+    Row(
+        modifier = modifier
+            .horizontalScroll(scrollState)
+            .shadow(1.dp)
+            .padding(vertical = 7.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        DropdownFilterChip(
+            text = "지역",// TODO: 클릭 이벤트 처리
+        )
+        DropdownFilterChip(
+            text = "품종",// TODO: 클릭 이벤트 처리
+        )
+        FilterChip(
+            text = "목격한 동물"
+        )
+        FilterChip(
+            text = "실종 동물"
+        )
+        FilterChip(
+            text = "보호중"
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeFilterPreview() {
+    HomeFilter()
+}
