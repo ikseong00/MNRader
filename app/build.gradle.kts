c3702fedc3650efb161e644822657d3cf17cac8f
SangHyun Jung <wjdtkdgus876@naver.com>
2025-05-30 16:54:02 +0900

    feat: Parcelize 의존성 추가 #3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.parcelize")
 }
 
 val properties = Properties().apply {
