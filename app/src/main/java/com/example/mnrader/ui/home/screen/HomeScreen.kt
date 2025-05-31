5085d8eeabcfa25ebccbba24833394d1aa74a990
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-05-31 23:53:51 +0900

    feat: 버튼 컴포넌트 구현 #11

--- app/src/main/java/com/example/mnrader/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/mnrader/ui/home/screen/HomeScreen.kt
@@ -4,15 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mnrader.ui.common.MNRaderButton
 import com.example.mnrader.ui.home.component.HomeFilter
 import com.example.mnrader.ui.home.component.HomeTopBar
+import com.example.mnrader.ui.home.component.MapComponent
 
 
 @Composable
@@ -20,6 +28,7 @@ fun HomeScreen(
     padding: PaddingValues,
 ) {
     val scrollState = rememberScrollState()
+    var isExpanded by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -29,6 +38,20 @@ fun HomeScreen(
         HomeTopBar()
         Spacer(Modifier.height(5.dp))
         HomeFilter()
+        Spacer(Modifier.height(15.dp))
+        MapComponent(
+            isExpanded = isExpanded
+        )
+        if (!isExpanded) {
+            MNRaderButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp),
+                text = "전체보기",
+            ) {
+                isExpanded = true
+            }
+        }
     }
 }
 
