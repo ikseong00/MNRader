527d145d8cfa963e9b740fdf47fb4035615a156e
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-06-01 19:28:44 +0900

    feat: 네이버 지도 서비스 구현 #13

---
+++ app/src/main/java/com/example/mnrader/data/service/NaverService.kt
@@ -0,0 +1,17 @@
+package com.example.mnrader.data.service
+
+import com.example.mnrader.data.dto.NaverGeocodingResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface NaverService {
+    @GET("map-reversegeocode/v2/gc")
+    suspend fun reverseGeocode(
+        @Header("X-NCP-APIGW-API-KEY-ID") clientId: String,
+        @Header("X-NCP-APIGW-API-KEY") clientSecret: String,
+        @Query("coords") coords: String, // "경도,위도" 형식
+        @Query("output") output: String = "json",
+        @Query("orders") orders: String = "roadaddr"
+    ): NaverGeocodingResponse
+} 
