2e8fc420797b17d41877cf0f3ba28e41dbc7b3a3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-06-01 18:11:58 +0900

    feat: 공공데이터포털용 레트로핏 클라이언트 생성 #13

---
+++ app/src/main/java/com/example/mnrader/data/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.example.mnrader.data
+
+import com.example.mnrader.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private val DATA_PORTAL_BASE_URL = BuildConfig.DATA_PORTAL_BASE_URL
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    private val dataPortalRetrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(DATA_PORTAL_BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
