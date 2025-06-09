3b22b5459571f66d7921a7f9764434f19b44a977
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-06-09 16:47:21 +0900

    feat: CommonTopBar 구현 #28

---
+++ app/src/main/java/com/example/mnrader/ui/common/CommonTopBar.kt
@@ -0,0 +1,63 @@
+package com.example.mnrader.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mnrader.ui.theme.MNRaderTheme
+
+@Composable
+fun CommonTopBar(
+    modifier: Modifier = Modifier,
+    hasBackButton: Boolean = true,
+    title: String = "",
+    onBack: () -> Unit = { }
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(52.dp),
+    ) {
+        if (hasBackButton) {
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .size(32.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ChevronLeft,
+                    contentDescription = "Back"
+                )
+            }
+        }
+        Text(
+            modifier = Modifier
+                .align(Alignment.Center),
+            text = title,
+            style = MNRaderTheme.typography.semiBold.copy(
+                fontSize = 16.sp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CommonTopBarPreview() {
+    CommonTopBar(
+        title = "Common Top Bar",
+        onBack = { /* Do nothing */ }
+    )
+}
